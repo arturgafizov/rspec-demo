ac40ceb455a18b41a0189d0b4c1f821543579f0d
Artur Gafizov <art05xxx@rambler.ru>
2020-01-23 07:55:25 +0300

    Add test for capitalized letter

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,5 +1,12 @@
-require './herq'
+require './hero'
 
 
-describe Hero do 
+describe Hero do
+
+	it "has a capitalized name" do
+		hero = Hero.new 'mike'
+
+		expect(hero.name).to eq 'Mike' # hero.name == 'Mike'
+	end	
+
 end	
